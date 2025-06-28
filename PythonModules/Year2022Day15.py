a8b4fc7cd94e41365c612fc94306e0aaf1da59e1
Matthew Mitchell <mazchoo@gmail.com>
2025-06-28 11:32:29 +0700

    Formatting

--- PythonModules/Year2022Day15.py
+++ PythonModules/Year2022Day15.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 
 import build.Release.AdventModule as m
 
+
 def show_state(sand: m.FallingSandGame):
     plt.imshow(falling_sand.image, cmap='plasma', vmin=0, vmax=3)
     plt.title(f'There are {falling_sand.nrSand} blocks of sand')
@@ -15,4 +16,3 @@ for i in range(1, 100000):
     falling_sand.updateUntilAtRest()
     if i % 1000 == 0:
         show_state(falling_sand.image)
-
